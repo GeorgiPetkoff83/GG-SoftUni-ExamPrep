3e552683c0823ce9758e01e9e1b3d8d66f0a8ebf
Georgi Georgiev <georgiev.georgi@siemens.com>
2024-11-23 17:41:29 +0200

    Initial commit

--- lib/exam_prep-stack.ts
+++ lib/exam_prep-stack.ts
@@ -1,4 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { PartitionKey } from 'aws-cdk-lib/aws-appsync';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Subscription, SubscriptionProtocol, Topic } from 'aws-cdk-lib/aws-sns';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +12,45 @@ export class ExamPrepStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+  const errorTable = new Table (this, 'ErrorTable', {
+    partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING
+    },
+    billingMode: BillingMode.PAY_PER_REQUEST,
+    timeToLiveAttribute: 'ttl'
+  });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ExamPrepQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  const errorTopic = new Topic(this, 'ErrorTopic',{
+    topicName: 'ErrorTopic'
+  });
+  
+  const processFunction = new NodejsFunction(this,'processFunction',{
+    runtime: Runtime.NODEJS_20_X,
+    handler: 'handler',
+    entry: '${__dirname}/../src/processFunction.ts',
+    environment: {
+      TABLE_NAME: errorTable.tableName,
+      TOPIC_ARN: errorTopic.topicArn
+    }
+  });
+  
+    errorTopic.grantPublish(processFunction);
+    errorTable.grantReadWriteData(processFunction);
+
+
+  const api = new RestApi(this, 'ProcessorApi');
+  const resource = api.root.addResource('processJSON');
+  resource.addMethod('POST', new  LambdaIntegration(processFunction));
+
+  new Subscription(this, 'ErrorSubscription', {
+    topic: errorTopic,
+    protocol: SubscriptionProtocol.EMAIL,
+    endpoint: 'petkoff83@gmail.com'
+  })
+
+  new cdk.CfnOutput(this, 'RestApiEndpoint',{
+    value: `https://${api.restApiId}.execute-api.eu-central-1.amazonaws.com/prod/processJSON`
+  })
   }
 }
---
+++ src/processFunction.ts
@@ -0,0 +1,48 @@
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { PublishCommand, SNSClient } from "@aws-sdk/client-sns"; 
+
+const snsClient = new SNSClient({});
+const dynamoDBClient = new DynamoDBClient({})
+
+
+export const handler =async (event: any) =>{
+    const tableName = process.env.TABLE_NAME;
+    const topicArn = process.env.OPIC_ARN;
+
+
+    console.log(event);
+
+    if(!event || !event.text){
+        // Invalid JSON
+        const ttl= Math.floor(Date.now()/1000)+ 30*60;
+        await dynamoDBClient.send(new PutItemCommand(
+            {
+                TableName: tableName,
+                Item: {
+                    id: {
+                        S: Math.random().toString(),
+                    },
+                    errorMessage: {
+                        S: 'Something is wrong!',
+                    },
+                    ttl: {
+                        N: ttl.toString(),
+                    }
+                }
+            }
+        ));
+    }else{
+        //Push to SNS
+        await snsClient.send(new PublishCommand(    {
+            TopicArn: topicArn,
+            Message: `Valid JSON recieved: ${event.text}`,
+
+        }
+        ))
+    }
+
+    return {
+        statusCode: 200,
+        body: 'Hello from Lambda AWS!'
+    }
+} 
